f3d87c5bed9e9d1bb1a128a4b20806faabc21c93
vicchi <gary@vicchi.org>
2012-06-01 15:46:53 +0100

    Add links for Google and Twitter authentication to the account page

--- www/account.php
+++ www/account.php
@@ -4,10 +4,28 @@
 	#
 
 	include("include/init.php");
+	
+	loadlib ('google_users');
+	loadlib ('twitter_users');
 
 	login_ensure_loggedin("/account");
 
-
+	$google_auth_status = 0;
+	$twitter_auth_status = 0;
+	
+	$google_user = google_users_get_by_user_id ($GLOBALS['cfg']['user']['id']);
+	if ($google_user['user_id']) {
+		$google_auth_status = 1;
+	}
+	
+	$twitter_user = twitter_users_get_by_user_id ($GLOBALS['cfg']['user']['id']);
+	if ($twitter_user['user_id']) {
+		$twitter_auth_status = 1;
+	}
+	
+	$smarty->assign ('has_google_account', $google_auth_status);
+	$smarty->assign ('has_twitter_account', $twitter_auth_status);
+	
 	#
 	# output
 	#
