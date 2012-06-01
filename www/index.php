cbbcb9a4fc75a9d02cda94cc1a11128c87d467a6
vicchi <gary@vicchi.org>
2012-06-01 14:48:56 +0100

    Strip out template logging and make /account the default landing page once logged in/authenticated

--- www/index.php
+++ www/index.php
@@ -1,33 +1,13 @@
 <?
-	#
-	# $Id$
-	#
 
 	include('include/init.php');
 
-
-	#
-	# this is so we can test the logging output
-	#
-
-	if ($_GET['log_test']){
-		log_error("This is an error!");
-		log_fatal("Fatal error!");
+	if (!$GLOBALS['cfg']['user']['id']) {
+		$GLOBALS['smarty']->display ('page_index_loggedout.txt');
+		exit ();
 	}
 
+	$url = '/account/';
+	header ('location: ' . $url);
 
-	#
-	# this is so we can test the HTTP library
-	#
-
-	if ($_GET['http_test']){
-		$ret = http_get("http://google.com");
-	}
-
-
-	#
-	# output
-	#
-
-	$smarty->display('page_index.txt');
 ?>
